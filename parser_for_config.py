5f49c700e64d70ffa21b1837f431e9679149506c
13dtran13 <dtran1994@yahoo.com>
2016-01-18 21:39:16 -0800

    Added Histtable,Plottable Generators

--- parser_for_config.py
+++ parser_for_config.py
@@ -2,6 +2,7 @@ import argparse
 import numpy 
 import os
 from collections import defaultdict
+from scipy import stats
 import subprocess
 
 #python parser_for_config.py ~/Documents/Research/Schroeder\ Research/Statistics-Pipeline/data/phenodata.txt
@@ -67,10 +68,14 @@ def addToDataMatrix(MAIN_config):
 		array_to_insert_into_matrix.append(cond1)
 		array_to_insert_into_matrix.append(cond2)
 		data_Matrix.append(array_to_insert_into_matrix)
+
 		if (instrument == "6400"):
 			array_to_insert_into_matrix.append( ExtractOneCondtion(folder_and_file, "Time", instrument) )
 		if (instrument == "6400XT"):
 			array_to_insert_into_matrix.append( ExtractOneCondtion(folder_and_file, "FTime", instrument) )
+		array_to_insert_into_matrix.append( ExtractOneCondtion(folder_and_file, "Ci", instrument) )
+		array_to_insert_into_matrix.append( ExtractOneCondtion(folder_and_file, "H2OR", instrument) )
+		array_to_insert_into_matrix.append( ExtractOneCondtion(folder_and_file, "Cond", instrument) )
 	return data_Matrix
 
 '''
@@ -90,8 +95,9 @@ def ExtractOneCondtion( folder_and_file, cond, instrument ):
 	if (instrument == "6400XT"):
 
 		#Iterate through file and get header
-		header_array = []
-		cond_array = []
+		header_array, cond_array, array_to_return = [], [], []
+	
+	#find the header line
 		with open(abs_path_file) as file:
 			for line in file:
 				if ( line[1:4] == "Obs" ):		
@@ -102,7 +108,6 @@ def ExtractOneCondtion( folder_and_file, cond, instrument ):
 		
 		#Get the column number associated with the provided input values
 		column_number = header_array.index('"'+str(cond)+'"')
-		array_to_return = []	
 		
 		for subarray in cond_array:
 			rawData = subarray[column_number].rstrip()
@@ -128,16 +133,6 @@ def ExtractOneCondtion( folder_and_file, cond, instrument ):
 		cond_array = numpy.loadtxt(abs_path_file,dtype=str,delimiter=',',skiprows=lineToStartOn+1,usecols=(column_number,))
 		return cond_array
 
-
-	# #if the instrument is 6400 and the current file being examined comes from 6400
-	# if (instrument == "6400"):
-	# 	cond_array = numpy.loadtxt(abs_path_file,dtype=str,delimiter=',',skiprows=lineToStartOn+1,usecols=(column_number,))
-	# 	return cond_array
-
-	# #if the instrument was 6400Xt and the current file is from 6400XT
-	# if (row[6] == "6400XT" and datafile == row[5] + "/" + row[6]):
-	# 	cond_array = numpy.loadtxt(abs_path_file,dtype=str,delimiter=',',skiprows=lineToStartOn+1,usecols=(column_number,))
-	# 	print cond_array
 '''
 Takes in Main_config and clusters all files in config based on plantid-cond1-cond2-CO2
 and returns a dictionary where keys are the plantid-cond1-cond2-CO2 and values are
@@ -213,11 +208,103 @@ def splitArray( CO2R_array, levels_CO2_array):
 				temp_index = i + 5
 				break
 	return return_index_array
+
 '''
 Call a shell command while in script
 '''
 def callShellCommand(input):
 	subprocess.Popen(input,shell = True).wait()
+
+'''
+
+'''
+def parseStaticsForHistTable(cluster_files, data_Matrix):
+
+	plotTable = []
+	
+	# loop through each clustering and get the common id 
+	# and the series of files 
+	for key, value in cluster_files.iteritems():
+		id_array = key.split("-")
+		plantid = id_array[0]
+		cond1 = id_array[1]
+		cond2 = id_array[2]
+		CO2_int_array = split_CO2_String_Array( id_array[3] )
+		
+		#for each file parse the Co2 and time parameters
+		for file in value:
+			data_array_per_file = getData(file, data_Matrix)
+			
+			time_periods = data_array_per_file[6]
+			exp_time = data_array_per_file[9]
+			stored_indexes = [0]
+			CO2R_array = data_array_per_file[3]
+			levels_CO2_array = data_array_per_file[5]
+			conduction_array = data_array_per_file[12]
+			#If no time was provided
+			if ( time_periods[0] == -1 ):
+				stored_indexes = splitArray( CO2R_array, levels_CO2_array)
+				stored_indexes.append(len(CO2R_array) -1 )
+			
+			#Else if time was provided
+			else:
+				
+				#Indexes array referring to the split of data with time data and temporary variable
+				previous_sec = float(0)
+				
+				#For loop to find indexes for each time setting
+				for i in range(0 , len(time_periods)-1):
+					
+					#The current instrument time setting duration (additive)
+					cur_period = time_periods[i]
+					
+					#Current seconds accounted from previous time slot as well and update previous seconds
+					seconds = float(cur_period*60) + float(previous_sec)
+					previous_sec = seconds
+
+					#Find the index of time-data that is closest to the purposed time value
+					index = findNearestTimeIndex(float(seconds),exp_time)
+					stored_indexes.append(index)
+
+				#Add the last index into array
+				stored_indexes.append(len(exp_time)-1)
+			
+			plotTable.append(buildHistTable(stored_indexes,conduction_array,exp_time,data_array_per_file))
+	return plotTable
+'''
+Find Histogram Table
+(plantid,0) (BLcond,1) (LinR([Blcond],2) ) ( array of P/Cond,3 ) (value of max, 4) 
+Input: a plantid, array of stored indexes, conductance array,
+Output: An array to write that should be written to file
+'''
+def buildHistTable(indexes_list,conduction_array,time_array,array_data_file):
+	
+	#array that will hold the separate conduction_array 
+	separated_data, separated_time, plotTable = [],[],[]
+	for i in range ( 1, len(indexes_list) ):
+		placeholder,time_hold = [], []
+		for k in range( indexes_list[i-1], indexes_list[i]):
+			placeholder.append(conduction_array[k])
+			time_hold.append(time_array[k])
+		separated_time.append(time_hold)
+		separated_data.append(placeholder)
+	
+	#Add plantid with folder
+	plotTable.append(array_data_file[0])
+	#Add baseline conductance
+	baseline_data = numpy.array(separated_data[0],dtype=numpy.float64)
+	plotTable.append(numpy.mean(baseline_data))
+	#Add the slop the of line, after computing linear regression
+	x, y = numpy.array(separated_time[0],dtype=numpy.float64), numpy.array(separated_data[0],dtype=numpy.float64)
+	slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+	plotTable.append(slope)
+
+	#Add max values for each time period
+	max_array = []
+	for time_period in separated_data:
+		max_array.append(max(time_period))
+	plotTable.append(max_array)
+	return plotTable
 
 '''
 Print the entire data matrix in a text art version
@@ -226,9 +313,9 @@ def toString(data_Matrix):
 	print "\n\n\n"
 	print "                                                           Table Values For Each File                                                                    "
 	print "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-	print "|\tFold-File\t|\t[]cond1\t|\t[]cond2\t|\t[]CO2\t|\tplantid\t|\t[]L.Co2\t|\t[]Time\t|\tcond1\t|\tcond2\t|\t[]time_exp\t|"
+	print "|\tFold-File\t|\t[]cond1\t|\t[]cond2\t|\t[]CO2\t|\tplantid\t|\t[]L.Co2\t|\t[]Time\t|\tcond1\t|\tcond2\t|\t[]time_exp\t|\t[]ci_exp\t|\t[]H2OR_exp\t|"
 	print "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"	
-	print "|\t0 index\t\t|\t1 index\t|\t2 index\t|\t3 index\t|\t4 index\t|\t5 index\t|\t6 index\t|\t7 index\t|\t8 index\t|\t 9 index\t|"
+	print "|\t0 index\t\t|\t1 index\t|\t2 index\t|\t3 index\t|\t4 index\t|\t5 index\t|\t6 index\t|\t7 index\t|\t8 index\t|\t 9 index\t|\t 10 index\t|\t 11 index\t|"
 	print "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 	print "_________________________________________________________________________________________________________________________________________________________________________________"
 
@@ -262,56 +349,11 @@ def main():
 
 	toString(data_Matrix)
 
-	# loop through each clustering and get the common id 
-	# and the series of files 
-	for key, value in cluster_files.iteritems():
-		id_array = key.split("-")
-		plantid = id_array[0]
-		cond1 = id_array[1]
-		cond2 = id_array[2]
-		CO2_int_array = split_CO2_String_Array( id_array[3] )
-		
-		for file in value:
-			data_array_per_file = getData(file, data_Matrix)
-			
-			time_periods = data_array_per_file[6]
-			exp_time = data_array_per_file[9]
-			stored_indexes = [0]
-			CO2R_array = data_array_per_file[3]
-			levels_CO2_array = data_array_per_file[5]
+	histTable = parseStaticsForHistTable(cluster_files, data_Matrix)
+	
+	for array in histTable:
+		print array
 
-			
-			#If no time was provided
-			if ( time_periods[0] == -1 ):
-				stored_indexes = splitArray( CO2R_array, levels_CO2_array)
-				stored_indexes.append(len(CO2R_array) -1 )
-			
-			#Else if time was provided
-			else:
-				
-				#Indexes array referring to the split of data with time data and temporary variable
-				previous_sec = float(0)
-				
-				#For loop to find indexes for each time setting
-				for i in range(0 , len(time_periods)-1):
-					
-					#The current instrument time setting duration (additive)
-					cur_period = time_periods[i]
-					
-					#Current seconds accounted from previous time slot as well and update previous seconds
-					seconds = float(cur_period*60) + float(previous_sec)
-					previous_sec = seconds
-
-					#Find the index of time-data that is closest to the purposed time value
-					index = findNearestTimeIndex(float(seconds),exp_time)
-					stored_indexes.append(index)
-
-				#Add the last index into array
-				stored_indexes.append(len(exp_time)-1)
-			# print
-			# print"------------------------"
-			# print file
-			print stored_indexes
 		# file_count = len(value) + 1
 		# counter = 1
 		# #Create a figure for this clustering that will hold everything
